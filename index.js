9e89eb5e1c77a6819f9d9bb804502ee0e833a333
matt <bednarskimat@sehs.net>
2017-05-09 09:56:45 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var Realm = require('realm');
+
+var app = express();
+
+let PostSchema ={
+    name: 'Post',
+    properties:{
+        timestamp: 'date',
+        title: 'string',
+        content: 'string'
+    }
+};
+
+var blogRealm = new Realm({
+    path: 'blog.realm',
+    schema: [PostSchema]
+});
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+   let posts = blogRealm.objects('Post').sorted('timestamp', true);
+  res.render('index.ejs', {posts: posts});
+});
+
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + "/write.html");
+});
+
+app.post('/write', function(req, res){
+    let title = req.body['title'],
+    content = req.body['content'],
+    timestamp = new Date();
+
+    blogRealm.write(() =>{
+        blogRealm.create('Post', {title: title, content: content, timestamp: timestamp});
+    });
+    res.sendFile(__dirname + '/write-complete.html');
+});
+
+
+app.listen(3000, function(){
+    console.log("Listening on port 3000");
+});
